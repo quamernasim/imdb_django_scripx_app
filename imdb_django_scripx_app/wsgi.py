b8a7fd3f32fada929e7db28f86c1d8099434e30a
quamernasim <quamer23nasim@yahoo.com>
2023-05-26 01:30:00 +0530

    check

--- imdb_django_scripx_app/wsgi.py
+++ imdb_django_scripx_app/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imdb_django_scripx_app.settings')
+print('==============================================================================')
 
 application = get_wsgi_application()
